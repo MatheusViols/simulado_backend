b0c139b382830b6f6e4d513498500e7f6164151e
MatheusViols <matheusviolss@gmail.com>
2026-03-27 11:19:54 -0300

    feat: Operações subtract, multiply, divide e tests

--- calculadora/calculator.py
+++ calculadora/calculator.py
@@ -4,13 +4,12 @@ class Calculator:
         return a + b
 
     def subtract(self, a: float, b: float) -> float:
-        # TODO(aluno): implementar subtracao.
-        raise NotImplementedError("Metodo subtract ainda nao foi implementado.")
+        return a - b
 
     def multiply(self, a: float, b: float) -> float:
-        # TODO(aluno): implementar multiplicacao.
-        raise NotImplementedError("Metodo multiply ainda nao foi implementado.")
+        return a * b
 
     def divide(self, a: float, b: float) -> float:
-        # TODO(aluno): implementar divisao e tratar divisao por zero.
-        raise NotImplementedError("Metodo divide ainda nao foi implementado.")
+        # if a == 0 or b == 0 :
+        #     raise ZeroDivisionError
+        return a / b
--- calculadora/tests.py
+++ calculadora/tests.py
@@ -8,4 +8,28 @@ class CalculatorTestCase(TestCase):
         calc = Calculator()
         self.assertEqual(calc.add(2, 3), 5)
 
+    def test_subtract(self):
+        calc = Calculator()
+        self.assertEqual(calc.subtract(10, 20), -10)
+        self.assertEqual(calc.subtract(5, 2), 3)
+        self.assertEqual(calc.subtract(10, -10), 20)
+
+
+    def test_multiply(self):
+        calc = Calculator()
+        self.assertEqual(calc.multiply(10, 20), 200)
+        self.assertEqual(calc.multiply(-10, 20), -200)
+        self.assertEqual(calc.multiply(-10, -20), 200)
+        self.assertEqual(calc.multiply(0, 10), 0)
+
+
+    def test_divide(self):
+        calc = Calculator()
+        self.assertEqual(calc.divide(20, 4), 5)
+        self.assertEqual(calc.divide(-10, 2), -5)
+        self.assertEqual(calc.divide(-10, -2), 5)
+
+        self.assertEqual(calc.divide(10, 0), ZeroDivisionError)
+        self.assertEqual(calc.divide(0, 10), ZeroDivisionError)
+
     # TODO(aluno): adicionar testes para subtract, multiply e divide.
